08d398a377a11d44c8e32fb91f85ef5af74a60cf
Boubacar FALL <Boubacar.Fall.Etu@univ-lemans.fr>
2026-02-01 16:43:58 +0100

    Ajout classe Gare

---
+++ src/main/java/fr/cours/info/tp/ferroviaire/cartographie/models/Gare.java
@@ -0,0 +1,20 @@
+package fr.cours.info.tp.ferroviaire.cartographie.models;
+
+public class Gare {
+    private String id;
+    private String nom;
+    private String ville;
+    private int nombreQuais;
+
+    public Gare(String id, String nom, String ville, int nombreQuais) {
+        this.id = id;
+        this.nom = nom;
+        this.ville = ville;
+        this.nombreQuais = nombreQuais;
+    }
+
+    public String getId() { return id; }
+    public String getNom() { return nom; }
+    public String getVille() { return ville; }
+    public int getNombreQuais() { return nombreQuais; }
+}
