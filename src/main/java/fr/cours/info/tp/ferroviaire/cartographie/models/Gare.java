0672977fa1e128749dd4b708d9878f213773ad76
Boubacar FALL <Boubacar.Fall.Etu@univ-lemans.fr>
2026-02-01 17:01:54 +0100

    Bob: ajout accessible

--- src/main/java/fr/cours/info/tp/ferroviaire/cartographie/models/Gare.java
+++ src/main/java/fr/cours/info/tp/ferroviaire/cartographie/models/Gare.java
@@ -5,6 +5,7 @@ public class Gare {
     private String nom;
     private String ville;
     private int nombreQuais;
+    private boolean accessible;
 
     public Gare(String id, String nom, String ville, int nombreQuais) {
         this.id = id;
